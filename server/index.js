9d7603e2461cd21b750978b03705310fca07ae58
ibrahim <ibrahim.belkhiria.info@gmail.com>
2019-12-01 19:09:20 +0100

    save

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
 // connect to DB 
 require('./database/connection');
 app.use(bodyParser.json());
 const usersRoutes = require('./routes/users');
-app.use('/users',usersRoutes);
+app.use('/api/users',usersRoutes);
 
-app.listen(PORT,()=>console.log('listen to port 3000'));
+app.listen(PORT,()=>console.log('listen to port 5000'));
