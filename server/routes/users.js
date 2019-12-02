ff36143258aa8a4c3ae403558e9d4cc590997a81
ibrahim <ibrahim.belkhiria.info@gmail.com>
2019-12-02 11:11:49 +0100

    saving

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-
 const User = require('../models/user');
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 // list users 
 router.get('/', async (req,res)=>{
     const users = await User.findAll();
@@ -48,17 +49,56 @@ router.put('/:id',async (req,res)=>{
     res.send(updatedUser);
 // also validate the request
 // verify existance
-})
+});
+
+// get one user 
+router.get('/:id',async (req,res)=>{
+    const user = await User.findOne({where:{id: req.params.id }});
+    console.log('calling get user');
+    res.status(200).send(user);
+// also validate the request
+// verify existance
+});
 
 // Delete a user 
 router.delete('/:id', async (req,res)=> {
     // verify existance 
     const user = await User.findOne({where:{id: req.params.id }});
+    const id = user.id;
     user.destroy({force:true});
-    res.send('user deleted');
-
+    res.send({id});
+    
 });
 
 // search users
+router.post('/search',async (req,res)=>{
+
+  const keyword = req.body.keyword.toLowerCase();
+   const users =  await User.findAll({
+        where: {
+        [Op.or]:[
+             { 
+            customernumber: { [Op.like]: '%' + keyword + '%' },
+             },{
+            username: { [Op.like]: '%' + keyword + '%' },
+             },
+             {
+            firstname: { [Op.like]: '%' + keyword + '%' },
+            },{
+            lastname: { [Op.like]: '%' + keyword + '%' },
+            },{
+            email: { [Op.like]: '%' + keyword + '%' },
+            }
+            ]
+        }
+
+    });
+
+    res.status(200).send(users);
+});
+
+
+
+
 
 module.exports = router;
